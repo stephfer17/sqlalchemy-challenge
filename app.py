477c41396d917cb5a2da690fa01ce0f58d7eaf65
stephfer17 <136030328+stephfer17@users.noreply.github.com>
2023-08-21 16:42:18 -0400

    finished the last part with comments

--- app.py
+++ app.py
@@ -45,7 +45,9 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start/end<br/>"
+        f"Please put a date in start and end yyyy-mm-dd<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
 
     )
 @app.route("/api/v1.0/precipitation")
@@ -94,8 +96,39 @@ filter(Measurement.date >= last_year_rain).all()
  recent_stations=list(np.ravel(recent_station))
  return jsonify(recent_stations)
  
- 
- 
+#AskBSC Helped me out 
+@app.route("/api/v1.0/<start>")
+def Start_date(start):
+    calc = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    results_calc = (session.query(*calc)).\
+    filter(func.strftime("%Y-%m-%d",Measurement.date)>=start).group_by(Measurement.date).all()
+     # to catch all the data 
+    start_date = []                       
+    for climate_starter in results_calc:
+        start_dates = {}
+        start_dates["Date"] = climate_starter[0]
+        start_dates["Low Temp"] = climate_starter[1]
+        start_dates["Avg Temp"] = climate_starter[2]
+        start_dates["High Temp"] = climate_starter[3]
+        start_date.append(results_calc)
+    return jsonify(start_dates)
+    
+@app.route("/api/v1.0/<start>/<end>")
+def end_date(start,end):
+    calc_end = [Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    results_end = (session.query(*calc_end)).\
+        filter(func.strftime("%Y-%m-%d"(Measurement.date)>=start)).\
+        filter(func.strftime("%Y-%m-%d"(Measurement.date)<=end)).group_by(Measurement.date).all()
+    #same thing but for end 
+    start_date = []                       
+    for climate_starter in results_calc:
+        start_dates = {}
+        start_dates["Date"] = climate_starter[0]
+        start_dates["Low Temp"] = climate_starter[1]
+        start_dates["Avg Temp"] = climate_starter[2]
+        start_dates["High Temp"] = climate_starter[3]
+        start_date.append(start_dates)
+    return jsonify(results_end)
  # for debugging   
 if __name__ == '__main__':
     app.run(debug=True) 
